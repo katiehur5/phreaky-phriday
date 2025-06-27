e6440c33c72390a0238250e36b0f50051783e206
katiehur5 <katiehur13@gmail.com>
2025-06-27 11:39:37 -0400

    baseUrl fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
   withCredentials: true,
 });
 
